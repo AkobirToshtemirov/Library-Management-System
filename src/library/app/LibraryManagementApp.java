8b40494a734f7c87cd59f953de1763ce27a124a1
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2023-05-24 12:18:56 +0500

    add classes of App

---
+++ src/library/app/LibraryManagementApp.java
@@ -0,0 +1,14 @@
+package library.app;
+
+public class LibraryManagementApp {
+    public static void main(String[] args) {
+
+    }
+}
+
+// Functionality = 1) adding books 2) removing books
+//                  3) registering patron 4) updating patron information
+//                  5) handling borrowing 6) returning of books
+
+
+//  Patron => a person who buys the goods or uses the services of a library
