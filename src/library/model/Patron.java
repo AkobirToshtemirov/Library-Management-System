d57608e21cf3ca27ddeee37044cc6d6dd47329f4
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2023-05-24 12:39:02 +0500

    add a class Patron which represents the patrons information

--- src/library/model/Patron.java
+++ src/library/model/Patron.java
@@ -1,4 +1,63 @@
 package library.model;
 
+import java.util.UUID;
+
 public class Patron {
+    private String name;
+    private String address;
+    private String email;
+
+    private final String uniqueID;
+
+//    String uniqueID = UUID.randomUUID().toString();
+
+    public Patron(String name, String address, String email) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.uniqueID = UUID.randomUUID().toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUiqueID() {
+        return uniqueID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+//    public void setUiqueID(String uiqueID) {
+//        this.uiqueID = uiqueID;
+//    }
+
+
+    @Override
+    public String toString() {
+        return "Patron{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", uniqueID='" + uniqueID + '\'' +
+                '}';
+    }
 }
