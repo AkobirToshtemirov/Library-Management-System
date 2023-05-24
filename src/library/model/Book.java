d1a6173ba31b959083d4cf9e18de4f7e49728868
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2023-05-24 12:29:39 +0500

    add a class Book which represents the books in the library

--- src/library/model/Book.java
+++ src/library/model/Book.java
@@ -1,4 +1,41 @@
 package library.model;
 
 public class Book {
+    private final String title;
+    private final String author;
+    private final int publicationYear;
+    private final boolean isAvailable; // status
+
+    public Book(String title, String author, int publicationYear, boolean isAvailable) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isAvailable = isAvailable;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publicationYear=" + publicationYear +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
 }
